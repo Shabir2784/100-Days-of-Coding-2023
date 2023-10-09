e8cef3a6667394482a62f9e4bc75037d68787d6c
Shabir2784 <shabir.a270804@gmail.com>
2023-10-09 21:24:28 +0800

    day1

---
+++ Day1.java
@@ -0,0 +1,17 @@
+
+package day1;
+
+
+public class Day1 {
+
+   
+    public static void main(String[] args) {
+        String nama = "shobir";
+        int umur = 19;
+        double berat = 56.7;
+        System.out.println ("nama = "+ nama);
+        System.out.println ("umur = "+ umur);
+        System.out.println ("berat = "+ berat ++);
+    }
+    
+}
