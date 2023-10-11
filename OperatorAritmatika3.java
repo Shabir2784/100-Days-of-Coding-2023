e338e258919d296adf848c328a733bc7cd4ad7ea
Shabir2784 <shabir.a270804@gmail.com>
2023-10-11 11:28:05 +0800

    day3

---
+++ OperatorAritmatika3.java
@@ -0,0 +1,23 @@
+package operatoraritmatika3;
+import java.util.Scanner;
+public class OperatorAritmatika3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int a, b, c;
+        System.out.print("masukkan a = ");
+        a = input.nextInt();
+        System.out.print("masukkan b = ");
+        b = input.nextInt();
+        c = a + b;
+        System.out.print(a + " + " + b + " = ");
+        System.out.println(c);
+        c = a - b;
+        System.out.println(a +" - " + b + " = " + c);
+                
+        
+        
+        
+    
+    }
+    
+}
