cd36c683b437faa3ed4e2db41919fd66ef3fbb2f
Shabir2784 <shabir.a270804@gmail.com>
2023-10-13 17:41:21 +0800

    day5

---
+++ MenghitungLuasDanKelilingPersegi.java
@@ -0,0 +1,28 @@
+
+package menghitung.luas.dan.keliling.persegi;
+
+public class MenghitungLuasDanKelilingPersegi {
+
+    public static void main(String[] args) {
+        //menghitung luas dan keliling persegi
+        /*
+        L = S * S
+        K = 4 * S
+        */
+        int S, L, K;
+        S = 12;
+        L = S * S;
+        K = 4 * S;
+        System.out.print("\nluas persegi \t\t: ");
+        System.out.println(L);
+        System.out.print("\nkeliling persegi \t: ");
+        System.out.println(K);
+        /*
+        \n
+        \t
+        */
+        
+
+    }
+    
+}
