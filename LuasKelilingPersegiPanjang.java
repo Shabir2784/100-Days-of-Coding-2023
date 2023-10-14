eca258bc442a20abc68b978468781dd27ef9db8c
Shabir2784 <shabir.a270804@gmail.com>
2023-10-14 17:52:03 +0800

    hari6

---
+++ LuasKelilingPersegiPanjang.java
@@ -0,0 +1,22 @@
+
+package luaskelilingpersegipanjang;
+
+public class LuasKelilingPersegiPanjang {
+
+    
+    public static void main(String[] args) {
+        // menghitung luas dan keliling persegi panjang
+        /*
+        luas persegi panjang = p * l
+        */
+        int p, l, L, K;
+        p = 2;
+        l = 5;
+        L = p * l;
+        K = 8 * (p + l);
+        System.out.println(L);
+        System.out.println(K);
+    
+    }
+    
+}
