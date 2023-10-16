2bc60fd8f4b9da561f4ceb0e68f6adf2ee372dff
Shabir2784 <shabir.a270804@gmail.com>
2023-10-16 17:27:08 +0800

    hari8

---
+++ For_hari8.java
@@ -0,0 +1,16 @@
+package for_hari.pkg8;
+public class For_hari8 {
+
+    public static void main(String[] args) {
+        for (int i = 10; i < 20; i+=1) {
+            System.out.println(i);    
+            
+            
+        }
+        
+        
+          
+     
+    }
+    
+}
