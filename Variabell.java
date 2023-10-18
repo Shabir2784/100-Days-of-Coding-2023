d2308d765d108e8547b2012e9150863260cf7969
Shabir2784 <shabir.a270804@gmail.com>
2023-10-18 19:59:15 +0800

    hari10

---
+++ Variabell.java
@@ -0,0 +1,25 @@
+package variabell;
+public class Variabell {
+
+    public static void main(String[] args) {
+        //integer (int)
+        int umur = 19;
+        System.out.println("Umur = "+umur + " "+"tahun");
+        //floating pint (float)
+        float berat = (float) 79.9;
+        System.out.println("Berat = "+berat +" "+ "kg");
+        //double floading pint(double)
+        double panjang = 56.7;
+        System.out.println("Panjang = "+panjang +" "+"m");
+        //String (String)
+        String nama = "Shobir";
+        System.out.println ("Nama : "+nama);
+        //Karakter (Char)
+        char gender = 'L';
+        System.out.println ("gender = "+gender);
+        //boolean (boolean)
+        boolean sehat = true;
+        System.out.println ("sehat = "+sehat);
+    }
+    
+}
