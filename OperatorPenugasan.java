6afb5f784809b8bf6f9995635a79b72585252815
Shabir2784 <shabir.a270804@gmail.com>
2023-10-21 20:15:20 +0800

    hari13

---
+++ OperatorPenugasan.java
@@ -0,0 +1,17 @@
+package operatorpenugasan;
+
+public class OperatorPenugasan {
+
+    public static void main(String[] args) {
+        int angka = 4;
+        angka +=3;
+        angka -=2;
+        angka *=2;
+        angka %=12;
+        angka /=2;
+               
+        System.out.println(angka);
+
+    }
+    
+}
