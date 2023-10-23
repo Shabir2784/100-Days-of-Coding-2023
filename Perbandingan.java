00a25193d1a1b03e9bd02bd3b4e23d17a526a8e5
Shabir <147009644+Shabir2784@users.noreply.github.com>
2023-10-23 21:44:31 +0800

    hari15

---
+++ Perbandingan.java
@@ -0,0 +1,26 @@
+package perbandingan;
+
+public class Perbandingan {
+
+    public static void main(String[] args) {
+        /*
+        ==
+        !=
+        >
+        >=
+        <
+        <=
+        */
+        int a = 20;
+        int b = 10;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+                
+    
+    }
+    
+}
