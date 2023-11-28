478937668746cdd10e5bba903a6a257f90fd9eb5
Shabir2784 <shabir.a270804@gmail.com>
2023-11-28 20:49:27 +0800

    hari51

---
+++ Array1Dimensi.java
@@ -0,0 +1,14 @@
+package array1dimensi;
+public class Array1Dimensi {
+    public static void main(String[] args) {
+        int a [] = new int[3];
+        a [0] = 5;
+        a [1] = 7;
+        a [2] = 10;
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        
+    }
+    
+}
