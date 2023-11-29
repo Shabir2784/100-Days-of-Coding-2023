9ee25bb1b21e6ad602ea3516182b24cbd2c4d26d
Shabir2784 <shabir.a270804@gmail.com>
2023-11-29 20:57:06 +0800

    hari52

---
+++ LoopingPadaArray.java
@@ -0,0 +1,13 @@
+package loopingpadaarray;
+public class LoopingPadaArray {
+    public static void main(String[] args) {
+        int a [] = {1,3,5,6,10,21};
+        int jumlah = a.length;
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println(a[i]);
+            
+        }
+        
+    }
+    
+}
