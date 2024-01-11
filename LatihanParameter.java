093ba1a1e8d6984b377d0165398934306cb3d7b7
Shabir2784 <shabir.a270804@gmail.com>
2024-01-11 21:00:37 +0800

    hari95

---
+++ LatihanParameter.java
@@ -0,0 +1,19 @@
+package latihanparameter;
+
+public class LatihanParameter {
+    public static void main(String[] args) {
+
+        Status("firaun","meninggal");
+        System.out.println(" ");
+        Status("Qarun","meninggal");
+        System.out.println(" ");
+        Status("Namrud","meninggal");
+        System.out.println(" ");
+        
+    }
+    static void Status (String nama_1, String status){
+        System.out.println("Nama : "+ nama_1);
+        System.out.println("Status : "+status);
+    
+    }
+}
