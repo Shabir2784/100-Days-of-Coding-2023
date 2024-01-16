f0a085ff38e52ab2b803eb16e15c4eb9ca1b3c7d
Shabir2784 <shabir.a270804@gmail.com>
2024-01-16 20:50:05 +0800

    hari 100

---
+++ Parameter_TheEnd.java
@@ -0,0 +1,20 @@
+package parameter_theend;
+
+public class Parameter_TheEnd {
+    public static int add (int X , int Y){
+        System.out.println( X + Y);
+        return 0;
+    }
+    public static void main(String[] args) {
+        int result_1 = add(1, 2);
+        int result_2 = add(3, 4);
+        int result_3 = add(5, 6);
+        
+        
+    }
+//    public static int add(int X , int Y){
+//        System.out.println(X + Y);
+//        return 0;
+//    }
+    
+}
